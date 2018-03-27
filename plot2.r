73eed9d3f5c319e78304dd33b4b4616d0c95a1f9
chad-coursera-2018 <richardmercadopasco.mexico@gmail.com>
2018-03-27 23:20:02 +0800

    plot 3 and image. some fixes on p1,p2

--- plot2.r
+++ plot2.r
@@ -3,9 +3,9 @@ setwd("C:\\Users\\Chad\\Desktop\\GITTED\\ExData_Plotting1")
 
 ## load data (only between 2007-02-01 and 2007-02-02)
 data <- read.table("C:\\Users\\Chad\\Desktop\\GITTED\\household_power_consumption.txt", header=T, sep = ";", colClasses = "character", nrows = 70000, na.strings = "?")
-
-## prepare row and column layout
 data <- rbind(subset(data, Date == "1/2/2007"), subset(data, Date == "2/2/2007"))
+
+## prepare date and time data
 datetime <- paste(data$Date, data$Time)
 data <- cbind(data, datetime)
 
--- plot3.r
+++ plot3.r
@@ -2,4 +2,32 @@
 setwd("C:\\Users\\Chad\\Desktop\\GITTED\\ExData_Plotting1")
 
 ## load data (only between 2007-02-01 and 2007-02-02)
-data <- read.table(pipe('findstr /B /R ^[1-2]/2/2007 "C:\\Users\\Chad\\Desktop\\GITTED\\household_power_consumption.txt"'),header=F, sep=';',as.is=F,na.strings=c('?'),stringsAsFactors=F)
+data <- read.table("C:\\Users\\Chad\\Desktop\\GITTED\\household_power_consumption.txt", header=T, sep = ";", colClasses = "character", nrows = 70000, na.strings = "?")
+data <- rbind(subset(data, Date == "1/2/2007"), subset(data, Date == "2/2/2007"))
+
+## prepare date and time data
+datetime <- paste(data$Date, data$Time)
+data <- cbind(data, datetime)
+
+## convert time format
+data$datetime = strptime(data$datetime, "%d/%m/%Y %H:%M:%S")
+
+## plot 
+
+# blank
+png("plot3.png", width=480, height=480)
+
+# plot each dataset
+with(data, plot(datetime, Sub_metering_1, ylab = "Energy sub metering", xlab = "", type = "l", lty = 1 ))
+
+# add lines
+lines(data$datetime, data$Sub_metering_2, col = "red")
+lines(data$datetime, data$Sub_metering_3, col = "blue")
+
+# add legend
+legend("topright", c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), lty = c(1,1,1), lwd = c(1.5,1.5,1.5), col = c("black","blue","red"))
+
+## output to file
+dev.copy(png, file = "plot3.png")
+dev.off()
+
